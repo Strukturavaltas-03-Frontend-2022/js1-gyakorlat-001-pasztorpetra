7f84b571d869756c05f800d2069b1aa8653a3a24
Petra <pasztor.petra1998@gmail.com>
2022-08-10 23:08:03 +0200

    modification

---
+++ js/person.js
@@ -0,0 +1,10 @@
+// 3.feladat
+'use strict';
+
+const personDataLog = ({
+    firstname: 'John',
+    lastname: 'Doe',
+    age: '33'
+}={}) => {
+    return 'My name is' + firstname + lastname + '. I'm' + age + 'years old.';
+};
